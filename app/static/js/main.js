514e0afcdfc747c5e37c8e54d2f63dc367b561d2
sarbjit87 <sarbjit@Sarbjits-MacBook-Air.local>
2020-05-22 09:49:35 -0400

    Dark theme support
    Added toggle button to switch between light and dark theme

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,8 +1,18 @@
-$(document).on('mouseup touchend', function (e) {
-  var container = $('.bootstrap-datetimepicker-widget');
-  if (!container.is(e.target) && container.has(e.target).length === 0) {
-    container.parent().datetimepicker('hide');
+if(localStorage.theme) {
+  $("link[id='styles']").attr('href', localStorage.theme);
+  if (localStorage.theme == "https://maxcdn.bootstrapcdn.com/bootswatch/4.5.0/journal/bootstrap.min.css") {
+    $("input[id='customSwitch1']").prop('checked', false);
+  }
+}
+
+$('#customSwitch1').click(function () {
+  if ($("link[id='styles']").attr('href') == 'https://stackpath.bootstrapcdn.com/bootswatch/4.5.0/journal/bootstrap.min.css'){
+    $("link[id='styles']").attr('href', 'https://maxcdn.bootstrapcdn.com/bootswatch/4.5.0/darkly/bootstrap.min.css');
+    localStorage.theme = "https://maxcdn.bootstrapcdn.com/bootswatch/4.5.0/darkly/bootstrap.min.css";
+    $("input[id='customSwitch1']").prop('checked', true);
+  } else {
+    $("link[id='styles']").attr('href', 'https://stackpath.bootstrapcdn.com/bootswatch/4.5.0/journal/bootstrap.min.css');
+    localStorage.theme = "https://maxcdn.bootstrapcdn.com/bootswatch/4.5.0/journal/bootstrap.min.css";
+    $("input[id='customSwitch1']").prop('checked', false);
   }
 });
-
-$('select').selectpicker();
